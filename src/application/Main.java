622420aafe3ac8552f437c638d1b4760999d728b
Son Nguyen <nguyenson120296@gmail.com>
2017-11-09 21:04:35 +0700

    Da tao giao dien rong. Son

---
+++ src/application/Main.java
@@ -0,0 +1,25 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("/application/MainFXML.fxml"));
+			primaryStage.setTitle("Music Player");
+			primaryStage.setScene(new Scene(root));
+			primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/application/MainController.java
@@ -0,0 +1,15 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class MainController implements Initializable{
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+	}
+	
+}
